2ead0f842f107dfb834925907613bcaa94707bbe
sunyoong <haert2010@naver.com>
2022-05-23 10:15:05 +0900

    계산기 Junit 테스트, submodule 추가

---
+++ CalculatorJunitTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorJunitTest
+{
+    @Test
+    public void add() {
+      Calculator cal = new Calculator();
+      System.out.println(cal.add(3, 1));
+    }
+    
+    @Test
+    public void substract() {
+        Calculator calculator = new Calculator();
+       System.out.println(calculator.substract(10, 5));
+    }
+
+    
+    // assertEquals를 활용한 테스트 방법
+    // 결과값이 다르면 오류, 결과값이 같으면 성공
+    @Test
+    public void add1() {
+        Calculator cal = new Calculator();
+        assertEquals(10, cal.add(2, 7));
+    }
+    
+    
+    
+}
